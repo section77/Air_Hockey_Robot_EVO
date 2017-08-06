4b2366109fec0ed586e6cfef34efd322c922c66e
j-keck <jhyphenkeck@gmail.com>
2017-08-06 16:01:36 +0200

    throttle maximum speed to prevent loosing steps
      - tested MAX_SPEED values:
        - 22000 -> works, but slow
        - 28000 -> loosing steps again
      - so i keep 27000
      - increment acceleration a little

--- Arduino/AHRobot_EVO/Configuration.h
+++ Arduino/AHRobot_EVO/Configuration.h
@@ -8,8 +8,8 @@
 // ABSOLUTE MAX ROBOT SPEED AND ACCELERATION
 // THIS VALUES DEPENDS ON YOUR ROBOT CONSTRUCTION (MOTORS, MECHANICS...)
 // RECOMMENDED VALUES FOR 12V POWER SUPPLY
-#define MAX_ACCEL 275           // Maximun motor acceleration in (steps/seg2)/1000. Max recommended value:280
-#define MAX_SPEED 32000         // Maximun speed in steps/seg. Max absolute value: 32767!!
+#define MAX_ACCEL 300           // Maximun motor acceleration in (steps/seg2)/1000. Max recommended value:280
+#define MAX_SPEED 27000         // Maximun speed in steps/seg. Max absolute value: 32767!!
 
 #define MIN_ACCEL 100
 #define MIN_SPEED 5000
